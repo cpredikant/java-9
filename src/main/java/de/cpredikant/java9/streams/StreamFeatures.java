37892ad9e3cdf0d4648ec5d05b28a688ae1d5da3
Christian Predikant <christian.predikant@gmail.com>
2018-01-08 21:12:08 +0100

    More Streams

--- src/main/java/de/cpredikant/java9/streams/StreamFeatures.java
+++ src/main/java/de/cpredikant/java9/streams/StreamFeatures.java
@@ -9,11 +9,14 @@ public class StreamFeatures {
 
     public void betterIteration(){
         IntStream.iterate(1, i -> i < 10, i -> i + 1).forEach(System.out::println);
+        System.out.println("----");
+        IntStream.iterate(1, i -> i < 10, i -> i + 1).takeWhile(i-> i < 5).forEach(System.out::println);
+        System.out.println("----");
+        IntStream.iterate(1, i -> i < 10, i -> i + 1).dropWhile(i-> i < 5).forEach(System.out::println);
     }
 
     public void optionalStreams(){
         Stream<Integer> s = Optional.of(1).stream();
-
         s.forEach((i)-> System.out.println(i));
     }
 }
